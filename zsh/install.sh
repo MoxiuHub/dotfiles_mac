3665e16fd992293e783ea348b1e284a5c22ed1b7
Moxiu <jonny6015@icloud.com>
2022-09-15 03:48:22 +0000

    Update.

--- zsh/install.sh
+++ zsh/install.sh
@@ -8,11 +8,12 @@ rm -rf ~/.zsh
 rm -rf ~/.zshenv
 rm -rf ~/.zprofile
 rm -rf ~/.profile
+rm -rf ~/.p10k.zsh
 
 mkdir -p ~/.zsh
-ln -s ./zprofile ~/.zprofile
-ln -s ./zshenv ~/.zshenv
-ln -s ./zshrc ~/.zshrc
-ln -s ./profile ~/.profile
-#ln -s ./p10k.zsh ~/.p10k.zsh
+ln -s ~/.zprofile ./.zprofile
+ln -s ~/.zshenv ./.zshenv
+ln -s ~/.zshrc ./.zshrc
+ln -s ~/.profile ./.profile
+ln -s ~/.p10k.zsh ./.p10k.zsh
 
