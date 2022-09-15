9d2fc24237724751c8b278b6878e2cd3a025adbb
Moxiu <jonny6015@icloud.com>
2022-09-15 04:47:25 +0000

    Update.

--- zsh/install.sh
+++ zsh/install.sh
@@ -1,19 +1,18 @@
 #!/bin/bash
 
 brew install exa
-apt -y install exa
 
-rm -rf ~/.zshrc
-rm -rf ~/.zsh
-rm -rf ~/.zshenv
-rm -rf ~/.zprofile
-rm -rf ~/.profile
-rm -rf ~/.p10k.zsh
+rm -rf ~/.zshrc ~/.zsh ~/.zshenv ~/.zprofile ~/.profile ~/.p10k.zsh
 
-mkdir -p ~/.zsh
-ln -s ~/.zprofile ./.zprofile
-ln -s ~/.zshenv ./.zshenv
-ln -s ~/.zshrc ./.zshrc
-ln -s ~/.profile ./.profile
-ln -s ~/.p10k.zsh ./.p10k.zsh
+ln -s $HOME/.dotfiles/zsh/zprofile ~/.zprofile
+ln -s $HOME/.dotfiles/zsh/zshenv ~/.zshenv
+ln -s $HOME/.dotfiles/zsh/zshrc ~/.zshrc
+ln -s $HOME/.dotfiles/zsh/profile ~/.profile
+ln -s $HOME/.dotfiles/zsh/p10k.zsh ~/.p10k.zsh
+
+git clone https://github.com/romkatv/powerlevel10k.git ${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/themes/powerlevel10k
+git clone https://github.com/MichaelAquilina/zsh-you-should-use.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/you-should-use
+git clone https://github.com/zsh-users/zsh-autosuggestions.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
+git clone https://github.com/lukechilds/zsh-nvm.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-nvm
+git clone https://github.com/z-shell/F-Sy-H.git ${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/plugins/F-Sy-H
 
