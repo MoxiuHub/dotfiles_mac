3b4ef54ffca29d2d9a4390c4e69b0f9031d2b667
Moxiu <jonny6015@icloud.com>
2022-09-15 03:11:10 +0000

    Update.

--- zsh/install.sh
+++ zsh/install.sh
@@ -3,11 +3,17 @@
 brew install exa
 apt -y install exa
 
+rm -rf ~/.zshrc
+rm -rf ~/.zsh
+rm -rf ~/.zshenv
+rm -rf ~/.zprofile
+#rm -rf ~/.profile
+
 mkdir -p ~/.zsh
 ln -s ./zalias ~/zsh/.zalias
-ln -s ./profile ~/zsh/.profile
 ln -s ./zprofile ~/.zprofile
 ln -s ./zshenv ~/.zshenv
 ln -s ./zshrc ~/.zshrc
+#ln -s ./profile ~/zsh/.profile
 # ln -s ./p10k.zsh ~/.p10k.zsh
 
